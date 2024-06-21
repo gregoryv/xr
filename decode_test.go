3b2122a83204a118af1373c96ed1c7dedf32ad16
Gregory Vincic <gregoryvse@gmail.com>
2024-06-21 08:34:49 +0200

    Test Decode

--- decode_test.go
+++ decode_test.go
@@ -3,8 +3,74 @@ package httpr
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+func TestDecode_stopOnFirstError(t *testing.T) {
+	var x struct {
+		First  bool `header:"first"`
+		Second bool `header:"second"`
+	}
+	r := httptest.NewRequest("GET", "/", http.NoBody)
+	r.Header.Set("first", "jibberish")
+	r.Header.Set("second", "true")
+	if err := Decode(&x, r); err == nil {
+		t.Error("expect error")
+	}
+	if x.Second {
+		t.Error("Second was set")
+	}
+}
+func TestDecode_contentType(t *testing.T) {
+	data := `{broken`
+	body := strings.NewReader(data)
+	r := httptest.NewRequest("GET", "/", body)
+	r.Header.Set("content-type", "application/json")
+
+	var x struct {
+		Name string `json:"name"`
+	}
+	if err := Decode(&x, r); err == nil {
+		t.Error("expect error")
+	}
+}
+
+func TestDecode_unsupported(t *testing.T) {
+	type complex struct {
+		Name string
+	}
+	var x struct {
+		C complex `header:"input"`
+	}
+	r := httptest.NewRequest("GET", "/", http.NoBody)
+	r.Header.Set("input", "not an int")
+	if err := Decode(&x, r); err == nil {
+		t.Error("expect error")
+	}
+}
+
+func TestDecode_bool(t *testing.T) {
+	var x struct {
+		I bool `header:"flag"`
+	}
+	r := httptest.NewRequest("GET", "/", http.NoBody)
+	r.Header.Set("flag", "not an int")
+	if err := Decode(&x, r); err == nil {
+		t.Error("expect error")
+	}
+}
+
+func TestDecode_atoi(t *testing.T) {
+	var x struct {
+		I int `header:"number"`
+	}
+	r := httptest.NewRequest("GET", "/", http.NoBody)
+	r.Header.Set("number", "not an int")
+	if err := Decode(&x, r); err == nil {
+		t.Error("expect error")
+	}
+}
 
 func TestDecode_missingSet(t *testing.T) {
 	defer catchPanic(t)
@@ -15,6 +81,17 @@ func TestDecode_missingSet(t *testing.T) {
 	r := httptest.NewRequest("GET", "/", http.NoBody)
 	r.Header.Set("authorization", "Bearer ...token...")
 	_ = Decode(&x, r)
+}
+
+func TestDecode_noValue(t *testing.T) {
+	var x struct {
+		token string `header:"authorization"`
+	}
+	r := httptest.NewRequest("GET", "/", http.NoBody)
+	// missing header
+	if err := Decode(&x, r); err != nil {
+		t.Error(err)
+	}
 }
 
 func catchPanic(t *testing.T) {
